2d42295e446d7c60b526c8d9fd304612d3de2f22
fraserbil <apskaft27@gmail.com>
2017-07-14 15:17:06 +0200

    Add spawn points for power ups

--- game/start.gd
+++ game/start.gd
@@ -4,5 +4,7 @@ func _ready():
 	set_process_input(true)
 	
 func _input(event):
+	randomize()
+	
 	if Input.is_key_pressed(KEY_S):
 		controller.new_game()
--- game/world/world-1.gd
+++ game/world/world-1.gd
@@ -1,6 +1,18 @@
 extends "res://game/world/world.gd"
 
+onready var p_spawn = get_node("powerup_spawn")
+onready var powerup = preload("res://game/item/powerup.tscn")
+
+
 var next_world_index = 2
 
 func _ready():
 	print("Loaded World 1")
+
+
+func _on_p_spawn_timeout():
+	var fire = powerup.instance()
+	add_child(fire)
+	var spawn_points = p_spawn.get_children()
+	var spawn_point = spawn_points[randi() % spawn_points.size()];	
+	fire.set_pos(spawn_point.get_pos())
